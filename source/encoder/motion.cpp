a1b5c9e15d61e604ece0da7d567c254ef43ed93e
L4cache <42721712+L4cache@users.noreply.github.com>
2025-07-11 19:04:24 +0800

    change where MV is clipped

--- source/encoder/motion.cpp
+++ source/encoder/motion.cpp
@@ -1596,20 +1596,6 @@ me_hex2:
     // check mv range for slice bound
     X265_CHECK(((bmv.y >= qmvmin.y) & (bmv.y <= qmvmax.y)), "mv beyond range!");
 
-    // just assuming there's some special treatment for zero MVs in the spec and the MVD is not coded, I don't actually know.
-    if (bmv.notZero())
-    {
-        MV clipmin((int32_t) -(1<<15),     (int32_t) -(1<<15));
-        MV clipmax((int32_t)  (1<<15) - 1, (int32_t)  (1<<15) - 1);
-        clipmin += qmvp;
-        clipmax += qmvp;
-        if (!bmv.checkRange(clipmin, clipmax))
-        {
-            bmv = bmv.clipped(clipmin, clipmax);
-            bcost = subpelCompare(ref, bmv, satd) + mvcost(bmv);
-        }
-    }
-
     x265_emms();
     outQMv = bmv;
     return bcost;
